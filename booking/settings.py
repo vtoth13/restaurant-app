a9c8aac22121acaadd554ed15469858565de758a
vtoth13 <snapviki13@gmail.com>
2024-10-30 19:05:22 +0100

    settings update

--- booking/settings.py
+++ booking/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
