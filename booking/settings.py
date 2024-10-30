9afcad1b682c410277c49986fef9c601bd1e893d
vtoth13 <snapviki13@gmail.com>
2024-10-30 19:20:45 +0100

    settings update

--- booking/settings.py
+++ booking/settings.py
@@ -27,8 +27,10 @@ DEBUG = False
 ALLOWED_HOSTS = ['restaurant-app-eh0v.onrender.com']
 
 
-SECURE_SSL_REDIRECT = False
-
+SECURE_SSL_REDIRECT = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
