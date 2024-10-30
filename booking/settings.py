46128bf5d26683e0c88cf8de720fd79870f95bde
vtoth13 <snapviki13@gmail.com>
2024-10-30 19:12:59 +0100

    settings update

--- booking/settings.py
+++ booking/settings.py
@@ -129,18 +129,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Ensure STATICFILES_DIRS is correctly set
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
