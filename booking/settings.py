6c695259d8a0915da6ccf0f306b332efc07c7420
vtoth13 <snapviki13@gmail.com>
2024-11-03 17:51:58 +0100

    settings file update

--- booking/settings.py
+++ booking/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = "django-insecure--9)nm*p#)uiv$y^)axj2_u6xv#dut__4%w%uhe$4^zyia#y!7v
 DEBUG = True
 
 ALLOWED_HOSTS = ['restaurant-app-eh0v.onrender.com']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://restaurant-app-eh0v.onrender.com',
+]
 
 
 
@@ -73,9 +75,7 @@ TEMPLATES = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://restaurant-app-eh0v.onrender.com',
-]
+
 
 WSGI_APPLICATION = "booking.wsgi.application"
 
